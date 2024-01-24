bb17a21a62b7427062775e3b3ce23bb9fd308bac
bxySo <bdqllT@gmail.com>
2024-01-24 08:32:44 +0800

    CI/patches: Sync changes & Optimize scripts (#124)
    * Sync this commit tiann/KernelSU@2bab388
    * Improve compatibility
    Tested kernel versions: 5.4, 4.19, 4.14, 4.9
    
    - CI: Sync changes
    
    Signed-off-by: bdqllW <bdqllT@gmail.com>

--- patches/patches.sh
+++ patches/patches.sh
@@ -1,50 +1,76 @@
+#!/bin/bash
 # Patches author: weishu <twsxtd@gmail.com>
 # Shell authon: xiaoleGun <1592501605@qq.com>
-# 20240107
+#               bdqllW <bdqllT@gmail.com>
+# Tested kernel versions: 5.4, 4.19, 4.14, 4.9
+# 20240123
 
-# fs/ changes
-## exec.c
-if [ -z "$(grep "ksu" fs/exec.c)" ]; then
-    sed -i '/static int do_execveat_common/i\extern bool ksu_execveat_hook __read_mostly;\nextern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,\n                        void *envp, int *flags);\nextern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,\n                         void *argv, void *envp, int *flags);' fs/exec.c
-    if grep -q "return __do_execve_file(fd, filename, argv, envp, flags, NULL);" fs/exec.c; then
-        sed -i '/return __do_execve_file(fd, filename, argv, envp, flags, NULL);/i\    if (unlikely(ksu_execveat_hook))\n        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);\n    else\n        ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);' fs/exec.c
-    else
-        sed -i '/if (IS_ERR(filename))/i\    if (unlikely(ksu_execveat_hook))\n        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);\n    else\n        ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);\n' fs/exec.c
+patch_files=(
+    fs/exec.c
+    fs/open.c
+    fs/read_write.c
+    fs/stat.c
+    drivers/input/input.c
+)
+
+for i in "${patch_files[@]}"; do
+
+    if grep -q "ksu" "$i"; then
+        echo "Warning: $i contains KernelSU"
+        continue
     fi
-fi
 
-## open.c
-if [ -z "$(grep "ksu" fs/open.c)" ]; then
-    if grep -q "long do_faccessat(int dfd, const char __user \*filename, int mode)" fs/open.c; then
-        sed -i '/long do_faccessat(int dfd, const char __user \*filename, int mode)/i\extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n			 int *flags);' fs/open.c
-    else
-        sed -i '/SYSCALL_DEFINE3(faccessat, int, dfd, const char __user \*, filename, int, mode)/i\extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n			 int *flags);' fs/open.c
-    fi
-    sed -i '/if (mode & ~S_IRWXO)/i\    ksu_handle_faccessat(&dfd, &filename, &mode, NULL);\n' fs/open.c
-fi
+    case $i in
 
-## read_write.c
-if [ -z "$(grep "ksu" fs/read_write.c)" ]; then
-    sed -i '/ssize_t vfs_read(struct file/i\extern bool ksu_vfs_read_hook __read_mostly;\nextern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,\n        size_t *count_ptr, loff_t **pos);' fs/read_write.c
-    sed -i '/if (unlikely(!access_ok(VERIFY_WRITE, buf, count)))/i\    if (unlikely(ksu_vfs_read_hook))\n        ksu_handle_vfs_read(&file, &buf, &count, &pos);' fs/read_write.c
-fi
+    # fs/ changes
+    ## exec.c
+    fs/exec.c)
+        sed -i '/static int do_execveat_common/i\#ifdef CONFIG_KSU\nextern bool ksu_execveat_hook __read_mostly;\nextern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,\n			void *envp, int *flags);\nextern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,\n				 void *argv, void *envp, int *flags);\n#endif' fs/exec.c
+        if grep -q "return __do_execve_file(fd, filename, argv, envp, flags, NULL);" fs/exec.c; then
+            sed -i '/return __do_execve_file(fd, filename, argv, envp, flags, NULL);/i\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_execveat_hook))\n		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);\n	else\n		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);\n	#endif' fs/exec.c
+        else
+            sed -i '/if (IS_ERR(filename))/i\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_execveat_hook))\n		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);\n	else\n		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);\n	#endif' fs/exec.c
+        fi
+        ;;
 
-## stat.c
-if [ -z "$(grep "ksu" fs/stat.c)" ]; then
-    if grep -q "int vfs_statx(int dfd, const char __user \*filename, int flags," fs/stat.c; then
-        sed -i '/int vfs_statx(int dfd, const char __user \*filename, int flags,/i\extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);' fs/stat.c
-        sed -i '/unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;/a\\n    ksu_handle_stat(&dfd, &filename, &flags);' fs/stat.c
-    else
-        sed -i '/int vfs_fstatat(int dfd, const char __user \*filename, struct kstat *stat,/i\extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);' fs/stat.c
-        sed -i '/if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |/i\\n    ksu_handle_stat(&dfd, &filename, &flag);\n' fs/stat.c
-    fi
-fi
+    ## open.c
+    fs/open.c)
+        if grep -q "long do_faccessat(int dfd, const char __user \*filename, int mode)" fs/open.c; then
+            sed -i '/long do_faccessat(int dfd, const char __user \*filename, int mode)/i\#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n			 int *flags);\n#endif' fs/open.c
+        else
+            sed -i '/SYSCALL_DEFINE3(faccessat, int, dfd, const char __user \*, filename, int, mode)/i\#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n			 int *flags);\n#endif' fs/open.c
+        fi
+        sed -i '/if (mode & ~S_IRWXO)/i\	#ifdef CONFIG_KSU\n	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);\n	#endif\n' fs/open.c
+        ;;
 
-# drivers/input changes
-## input.c
-if [ -z "$(grep "ksu" drivers/input/input.c)" ]; then
-    sed -i '/static void input_handle_event/i\extern bool ksu_input_hook __read_mostly;\nextern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);' drivers/input/input.c
-    sed -i '/if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)/i\    if (unlikely(ksu_input_hook))\n        ksu_handle_input_handle_event(&type, &code, &value);' drivers/input/input.c
-fi
+    ## read_write.c
+    fs/read_write.c)
+        sed -i '/ssize_t vfs_read(struct file/i\#ifdef CONFIG_KSU\nextern bool ksu_vfs_read_hook __read_mostly;\nextern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,\n		size_t *count_ptr, loff_t **pos);\n#endif' fs/read_write.c
+        sed -i '/ssize_t vfs_read(struct file/,/ssize_t ret;/{/ssize_t ret;/a\
+        #ifdef CONFIG_KSU\
+        if (unlikely(ksu_vfs_read_hook))\
+            ksu_handle_vfs_read(&file, &buf, &count, &pos);\
+        #endif
+        }' fs/read_write.c
+        ;;
 
-echo "Patch kernel is ok"
+    ## stat.c
+    fs/stat.c)
+        if grep -q "int vfs_statx(int dfd, const char __user \*filename, int flags," fs/stat.c; then
+            sed -i '/int vfs_statx(int dfd, const char __user \*filename, int flags,/i\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif' fs/stat.c
+            sed -i '/unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;/a\\n	#ifdef CONFIG_KSU\n	ksu_handle_stat(&dfd, &filename, &flags);\n	#endif' fs/stat.c
+        else
+            sed -i '/int vfs_fstatat(int dfd, const char __user \*filename, struct kstat \*stat,/i\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif\n' fs/stat.c
+            sed -i '/if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |/i\	#ifdef CONFIG_KSU\n	ksu_handle_stat(&dfd, &filename, &flag);\n	#endif\n' fs/stat.c
+        fi
+        ;;
+
+    # drivers/input changes
+    ## input.c
+    drivers/input/input.c)
+        sed -i '/static void input_handle_event/i\#ifdef CONFIG_KSU\nextern bool ksu_input_hook __read_mostly;\nextern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);\n#endif\n' drivers/input/input.c
+        sed -i '/int disposition = input_get_disposition(dev, type, code, &value);/a\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_input_hook))\n		ksu_handle_input_handle_event(&type, &code, &value);\n	#endif' drivers/input/input.c
+        ;;
+    esac
+
+done
